209114814e4a0da87d50e3a70d0909cc20954af6
Owall <97934878+MayOwall@users.noreply.github.com>
2022-11-05 21:53:26 +0900

    #3.1 GET Requests

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 
+const PORT = 4000;
 const app = express();
 
 const handleListening = () => {
     console.log("Server is listening on port 4000 🚀");
 };
 
-app.listen(4000, handleListening);
+app.listen(PORT, handleListening);
