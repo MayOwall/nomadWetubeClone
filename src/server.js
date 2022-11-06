b9ff8453dbee9297603c42af62d3a9f131fd511b
Owall <97934878+MayOwall@users.noreply.github.com>
2022-11-06 16:46:32 +0900

    #3.10 Middleware Recap

--- src/server.js
+++ src/server.js
@@ -4,11 +4,28 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const home = (req, res) =>
-    res.send("<h1>Hello, welcome to homepage \\( ˙▿˙ )/</h1>");
+const routerLogger = (req, res, next) => {
+    console.log(`✅ : Path is ${req.path}`);
+    next();
+};
 
+const methodLogger = (req, res, next) => {
+    console.log(`✅ : Method type is ${req.method}`);
+    next();
+};
+
+// 관습적으로 middleware가 아닌 마지막 controller에는 next()를 적어주지 않는다.
+const home = (req, res) => {
+    res.send("<h1>Hello, Welcome to homepage \\( ˙▿˙ )/</h1>");
+};
+
+const login = (req, res) => {
+    res.send(`<h1>Hello, Welcome to login page ( ´ ▽ \` )ﾉ</h1>`);
+};
 const handleListening = () =>
     console.log(`🚀 : Server is listening on port ${PORT} (ว˙∇˙)ง`);
 
+app.use(methodLogger, routerLogger);
 app.get("/", home);
+app.get("/login", login);
 app.listen(PORT, handleListening);
