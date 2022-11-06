a60ba61e21043703e202ff81bd418098629f1e34
Owall <97934878+MayOwall@users.noreply.github.com>
2022-11-06 15:47:55 +0900

    #3.5 Middlewares part One

--- src/server.js
+++ src/server.js
@@ -3,9 +3,18 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+    console.log("I'm on the middle");
+    next();
+};
+
 const handleHome = (req, res) => {
     console.log("someone is approaching to home");
     return res.send({ message: "hello from here" });
+};
+
+const handleNext = (req, res) => {
+    return res.end();
 };
 
 const handleListening = () => {
@@ -13,4 +22,6 @@ const handleListening = () => {
 };
 
 app.get("/", handleHome);
+app.get("/next", gossipMiddleware, handleNext);
+
 app.listen(PORT, handleListening);
