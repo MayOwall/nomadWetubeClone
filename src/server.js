805a5323e4fd38a5209103ff6f46c14392d65bd4
Owall <97934878+MayOwall@users.noreply.github.com>
2022-11-06 16:09:58 +0900

    #3.7 Setup Recap

--- src/server.js
+++ src/server.js
@@ -8,13 +8,6 @@ const logger = (req, res, next) => {
     console.log(`${req.method}, ${req.url}`);
     next();
 };
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if (url === "/protected") {
-        return res.send("<h1>This page is not allowed.</h1>");
-    }
-    next();
-};
 
 // Controllers
 const handleHome = (req, res) => {
@@ -22,16 +15,11 @@ const handleHome = (req, res) => {
     return res.send({ message: "hello from here" });
 };
 
-const handleProtected = (req, res) => {
-    return res.send("<h1>Welcome to the private lounge</h1>");
-};
-
 const handleListening = () => {
     console.log(`Server is listening on port ${PORT} 🚀`);
 };
 
-app.use(logger, privateMiddleware);
+app.use(logger);
 app.get("/", handleHome);
-app.get("/protected", handleProtected);
 
 app.listen(PORT, handleListening);
