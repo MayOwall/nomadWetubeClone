88359799ebd3bc9d8bc78b8c90d4a0218b0648ea
Owall <97934878+MayOwall@users.noreply.github.com>
2022-11-06 14:43:32 +0900

    #3.3 Responses

--- src/server.js
+++ src/server.js
@@ -3,9 +3,14 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const handleHome = (req, res) => {
+    console.log("someone is approaching to home");
+    return res.send("hello from server to home");
+};
+
 const handleListening = () => {
     console.log(`Server is listening on port ${PORT} 🚀`);
 };
 
-app.get("/", () => console.log("hello I'm the answer for /GET"));
+app.get("/", handleHome);
 app.listen(PORT, handleListening);
