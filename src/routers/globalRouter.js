3dabf140d3231b1c1ec6896c55a1ec7332fc5896
Owall <97934878+MayOwall@users.noreply.github.com>
2022-11-06 18:24:23 +0900

    #4.5 Architecture Recap

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -3,6 +3,7 @@ import { handleJoin } from "../controllers/userController";
 
 const globalRouter = express.Router();
 
+// 홈페이지 컨트롤러는 어디에 두는게 맞으려나..,,
 const handleHome = (req, res) => {
     res.send("<h1>Welcome to page global-home ( ⸝⸝•ᴗ•⸝⸝ )੭⁾⁾</h1>");
 };
