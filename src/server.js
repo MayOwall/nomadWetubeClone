6e7bacaefeae1c10d3f4c87185c8614d2f5543da
Owall <97934878+MayOwall@users.noreply.github.com>
2022-11-06 14:01:46 +0900

    #3.2 GET Requests part Two

--- src/server.js
+++ src/server.js
@@ -4,7 +4,8 @@ const PORT = 4000;
 const app = express();
 
 const handleListening = () => {
-    console.log("Server is listening on port 4000 🚀");
+    console.log(`Server is listening on port ${PORT} 🚀`);
 };
 
+app.get("/", () => console.log("hello I'm the answer for /GET"));
 app.listen(PORT, handleListening);
