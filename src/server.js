c8078b3d4a95075926e663f3ba0019d0df2c10ce
MayOwall <pixartive@gmail.com>
2023-01-18 18:18:31 +0900

    #3.11 External Middlewares

--- src/server.js
+++ src/server.js
@@ -1,18 +1,8 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`Someone is ${req.method} > "${req.url}"`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") return res.send("<h1>Not allowed page</h1>");
-  next();
-};
 
 const handleHome = (req, res) => {
   return res.send("<h1>Hello, this is <u>HOME</u> page</h1>");
@@ -22,7 +12,7 @@ const handleLogin = (req, res) => {
   return res.send("<h1>hello, this is <u>LOGIN</u> page</h1>");
 };
 
-app.use(logger, privateMiddleware);
+app.use(morgan("dev"));
 app.get("/", handleHome);
 app.get("/login", handleLogin);
 
