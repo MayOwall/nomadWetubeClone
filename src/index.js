c4a6a854cae68b621c52193ea1ebfaa564c49601
MayOwall <pixartive@gmail.com>
2023-01-18 17:59:47 +0900

    #3.6 Middlewares part Two

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log("I'm in the middle!");
+const logger = (req, res, next) => {
+  console.log(`Someone is ${req.method} > "${req.url}"`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") return res.send("<h1>Not allowed page</h1>");
   next();
 };
 
@@ -16,7 +22,8 @@ const handleLogin = (req, res) => {
   return res.send("<h1>hello, this is <u>LOGIN</u> page</h1>");
 };
 
-app.get("/", gossipMiddleware, handleHome);
+app.use(logger, privateMiddleware);
+app.get("/", handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () =>
