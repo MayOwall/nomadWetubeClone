34213bd9fad30440efc37da9284a30b7d936c087
MayOwall <pixartive@gmail.com>
2023-01-18 17:47:57 +0900

    #3.5 Middlewares part One

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+  console.log("I'm in the middle!");
+  next();
+};
+
 const handleHome = (req, res) => {
   return res.send("<h1>Hello, this is <u>HOME</u> page</h1>");
 };
@@ -11,7 +16,7 @@ const handleLogin = (req, res) => {
   return res.send("<h1>hello, this is <u>LOGIN</u> page</h1>");
 };
 
-app.get("/", handleHome);
+app.get("/", gossipMiddleware, handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () =>
