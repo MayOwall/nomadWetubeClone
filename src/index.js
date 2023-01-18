9efdcccb744070672ffaf554c7fa99e0c11b52e9
MayOwall <pixartive@gmail.com>
2023-01-18 17:06:03 +0900

    #3.2 GET Requests part Two

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+const handleHome = () => console.log("GET / is required");
+
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`Server listening on port ${PORT} 🚀`);
 
