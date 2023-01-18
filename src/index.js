3dfab245e173bf70c136426d63ae5faf657e4960
MayOwall <pixartive@gmail.com>
2023-01-18 17:19:10 +0900

    #3.3 Responses

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
-const handleHome = () => console.log("GET / is required");
+const handleHome = (req, res) => {
+  return res.send("Hello, this is home page");
+};
+
+const handleLogin = (req, res) => {
+  return res.send("hello, this is login page");
+};
 
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`Server listening on port ${PORT} 🚀`);
