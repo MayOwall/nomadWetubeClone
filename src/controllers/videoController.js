25cb7de6b6f3110d929834ed36c990457efc3bf7
MayOwall <pixartive@gmail.com>
2023-01-19 17:19:10 +0900

    #4.7 URL Parameters part One

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,6 @@
-export const handleSee = (req, res) => res.send("VIDEO - SEE");
+export const handleSee = (req, res) => {
+  return res.send(`See Video #${req.params.id}`);
+};
 export const handleSearch = (req, res) => res.send("SEARCH");
 export const handleUpload = (req, res) => res.send("VIDEO - WATCH");
 export const handleEdit = (req, res) => res.send("VIDEO - EDIT");
