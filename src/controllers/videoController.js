7a6217c7056eab38ec2324c82b30bbaada8e7133
MayOwall <pixartive@gmail.com>
2023-02-02 18:26:51 +0900

    #6.27 Search part Two

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -71,8 +71,13 @@ export const deleteVideo = async (req, res) => {
 
 export const search = async (req, res) => {
   const { keyword } = req.query;
+  let videos = [];
   if (keyword) {
-    // search
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
+    });
   }
-  return res.render("search", { pageTitle: "SEARCH🔎" });
+  return res.render("search", { pageTitle: "SEARCH🔎", videos });
 };
