8909a9d7f9e94cb802ae56ddadb4ae5b098e088d
MayOwall <pixartive@gmail.com>
2023-02-02 18:16:18 +0900

    #6.26 Search part One

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { handleJoin, handleLogin } from "../controllers/userController";
-import { getHome, handleSearch } from "../controllers/videoController";
+import { getHome, search } from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", getHome);
 globalRouter.get("/join", handleJoin);
 globalRouter.get("/login", handleLogin);
+globalRouter.get("/search", search);
 
 export default globalRouter;
